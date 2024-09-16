65cfecce9e9a1fe8ecf37ec5db114c75643370f8
Jordy van Zeeland <jordyvanzeeland@gmail.com>
2024-09-16 17:34:53 +0200

    auth routes fix

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,9 @@ Route::group([
     'prefix' => 'auth'
 ], function ($router) {
     Route::post('login', 'App\Http\Controllers\AuthController@login');
-    Route::post('logout', 'AuthController@logout');
-    Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'AuthController@me');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
 });
 
 /**
